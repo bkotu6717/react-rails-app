7b619be2438f26045508f2302b94babca02d33b3
Devaraju <devarajuboddu@railsfactory.org>
2016-12-26 19:32:39 +0530

    delete contact model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,8 @@
 class Event < ApplicationRecord
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :locations
-	has_and_belongs_to_many :contacts
 
-	validates_presence_of :title, :description, :starts_at, :ends_at, :locations, :contacts
+	validates_presence_of :title, :description, :starts_at, :ends_at, :locations
 	# Custom validations
 	validate :validate_start_and_end_date
 
--- db/migrate/20161226111409_create_events_contacts.rb
+++
@@ -1,8 +0,0 @@
-class CreateEventsContacts < ActiveRecord::Migration[5.0]
-  def change
-     create_table :contacts_events, id: false do |t|
-      t.belongs_to :event, index: true
-      t.belongs_to :contact, index: true
-    end
-  end
-end
