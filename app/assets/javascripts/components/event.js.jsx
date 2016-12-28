a99794e2c28e5d263ee17e0af05a2efa0e534860
railsfactory-bhaskararao <bhaskararao@railsfactory.org>
2016-12-28 13:04:37 +0530

    Display date in proper format

--- app/assets/javascripts/components/event.js.jsx
+++ app/assets/javascripts/components/event.js.jsx
@@ -12,8 +12,8 @@ var Event = React.createClass({
         <td> {this.state.event.id} </td>
         <td> {this.state.event.title} </td>
         <td> {this.state.event.description} </td>
-        <td> {this.state.event.starts_at} </td>
-        <td> {this.state.event.ends_at} </td>
+        <td> <Formatdate event_date={this.state.event.starts_at} /> </td>
+        <td> <Formatdate event_date={this.state.event.ends_at} /> </td>
         <td>
          {(this.state.locations.length > 0) ? <Locations locations={this.state.locations}/> : 'No Location specified'}
         </td>
